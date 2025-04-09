fa9ccf11f869326c9419a40ed77cca9e3d335190
Ihor Kliushnyk <igorator746@gmail.com>
2025-04-09 18:09:58 +0300

    Refactor Navbar and Avatar components: update background color and enhance Avatar logic; adjust padding in PageWrapper

--- app/shared/components/telegram/Avatar.jsx
+++ app/shared/components/telegram/Avatar.jsx
@@ -1,14 +1,18 @@
 import { useEffect, useState } from 'react';
 
 import avatarPlaceholder from '../../../shared/assets/images/avatar-placeholder.webp';
-import { cn } from '../../../shared/utils/cn';
+import { cn } from '../../utils/cn';
 
 export const Avatar = () => {
   const [avatarUrl, setAvatarUrl] = useState(null);
 
   useEffect(() => {
-    if (window.TelegramWebApp && window.TelegramWebApp.initDataUnsafe) {
-      const user = window.TelegramWebApp.initDataUnsafe.user;
+    if (window.TelegramWebApp) {
+      const telegramWebApp = window.TelegramWebApp;
+
+      telegramWebApp.init();
+
+      const user = telegramWebApp.initData?.user;
 
       if (user && user.photo_url) {
         setAvatarUrl(user.photo_url);
@@ -16,7 +20,6 @@ export const Avatar = () => {
     }
   }, []);
 
-  // Если аватар не найден, используем изображение-заполнитель
   const imageUrl = avatarUrl || avatarPlaceholder;
 
   return (
